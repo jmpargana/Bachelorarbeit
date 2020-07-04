766e04fa3859c8f1bff2af6e57f31a35cba707c1
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-07-04 17:58:17 +0200

    yarn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import {Router, Route, Switch} from "react-router-dom";
 import history from './utils/history';
-import { useAuth0 } from "./helpers/react-auth0-spa";
+import {useAuth0} from "./helpers/react-auth0-spa";
 import Topic from "./components/Topic";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import TopicSelection from "./components/TopicSelection";
 import Spinner from './components/Spinner';
 import PrivateRoute from './components/PrivateRoute';
-import { ThemeProvider } from '@material-ui/core/styles';
+import {ThemeProvider} from '@material-ui/core/styles';
 import theme from './styles';
 
 export default function App() {
-  const { loading } = useAuth0();
+  const {loading} = useAuth0();
 
   if (loading) {
     return <Spinner />;
